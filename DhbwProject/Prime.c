3324cdddf7a945123842ef73c01f0ca1eb27e264
Erik <erik.kaufmann91@gmail.com>
2022-01-26 22:25:49 +0100

    backup2

--- DhbwProject/Prime.c
+++ DhbwProject/Prime.c
@@ -9,9 +9,9 @@
 #include "Helper.h"
 #include <windows.h>
 
-#define MAX_THREADS 1
+#define MAX_THREADS 2
 //#define MAX_NUMBER 100000000 // 100.000.000
-int MAX_NUMBER = 10000;
+int MAX_NUMBER = 100000;
 
 typedef struct Array
 {
@@ -63,24 +63,29 @@ int contains(Array* a, int key)
 	return 0;
 }
 
-struct param
+typedef struct param
 {
 	void* workerId;
+	int* from;
+	int* til;
+	int* max;
+	int* _numbers;
+	int* noPrimes;
 	int* counter;
-};
+} *PMYDATA;
 
-//void* WorkerFunction(struct param* param)
-//{
-//	long* myId = (long*)param->workerId;
-//	int counter = (int*)param->counter;
-//
-//	for (int i = 1; i <= counter; i++)
-//	{
-//		printf("\n%2ld (%d run) wird ausgeführt. Sleeping\n", *myId, i);
-//		Sleep(2000);
-//		printf("%2ld (%d run) wurde   beendet.\n", *myId, i);
-//	}
-//}
+void* WorkerFunction(struct param* param)
+{
+	long* myId = (long*)param->workerId;
+	int counter = (int*)param->counter;
+
+	for (int i = 1; i <= counter; i++)
+	{
+		printf("\n%2ld (%d run) wird ausgeführt. Sleeping\n", *myId, i);
+		Sleep(2000);
+		printf("%2ld (%d run) wurde   beendet.\n", *myId, i);
+	}
+}
 
 void InitializeNumbers(int* _numbers)
 {
@@ -91,78 +96,196 @@ void InitializeNumbers(int* _numbers)
 	}
 }
 
-int CreateErathotenes(int* _numbers)
+
+/// <param name="from"></param>
+/// <param name="til"></param>
+/// <param name="_numbers"></param>
+/// <param name="noPrimes"></param>
+//void calcPrimes(int* from, int* til, int* _numbers, int* noPrimes)
+DWORD WINAPI calcPrimes(struct param* param)
 {
-	int noPrimeNumbers = 0;
-	int primeNumbers = -2;
-	int maxVielfaches = MAX_NUMBER;
+	int* from = (int*)param->from;
+	int* til = (int*)param->til;
+	int* _numbers = (int*)param->_numbers;
+	int* noPrimes = (int*)param->noPrimes;
+	int* max = (int*)param->max;
+	int zero = 0;
 
-	int flag = 1; // still looking for prime
+	double time_spent = 0.0;
+	clock_t begin = clock();
 
-	struct Array bl;
-	initArray(&bl, MAX_NUMBER);
+	int teilerFound = 0;
+	int highestTeiler = 2;
 
-	bl.array[0] = 2;
-	bl.used++;
-
-	// Durchlaufe jede Zahl bis MAX
-	for (int i = 0; i < MAX_NUMBER; i++)
+	for (int i = *from; i >= *from && i <= *til; i++)
 	{
-		//printf("Prüfe Index %d --> ", i);
-		flag = 1;
-
-		if (contains(&bl, i) == 1) // Aktuelle Zahl ist in der Blacklist enthalten --> No Prime
+		if (_numbers[i] == 1)
 		{
-			//printf(" %d ist in Blacklist enthalten  --> no Prime \n", i);
-			//_numbers[i] = 0;
-			flag = 0;
-		}
-
-		int teiler = bl.used + 1;
-
-		while (teiler < i && teiler > 0 && flag == 1)
-		{
-			if (i % teiler == 0)
+			for (int k = highestTeiler; k < MAX_NUMBER && k < i; k++)
 			{
-				bl.array[bl.used++] = i;
-				flag = 0;
-				//printf(" %d wird geteilt von teiler %d und somit ist %d keine Primzahl --> \n", i, teiler, i);
-				// Füge Blacklist weitere Teiler hinzu 3->6->9->12 etc
+				if (_numbers[k] == 1 && i % k == 0 && i > highestTeiler) // k teilt i z.B. 2 teilt 4
+				{
+					teilerFound = 1;
+					highestTeiler = k;
+
+					for (int f = i; f < MAX_NUMBER; f = f + k)
+					{
+						if (_numbers[f] == 1)
+						{
+							_numbers[f] = zero;
+							*noPrimes = *noPrimes + 1;
+						}
+					}
+				}
 			}
-			teiler++;
 		}
 
-		//// Durchlaufe jedes Vielfaches
-		//for (int vielFaches = bl.used +1;
-		//	vielFaches < i 
-		//	&& vielFaches > 0 
-		//	&& flag == 1; vielFaches++) // Sieb für 2,3,4
-		//{
-		//	if (i % vielFaches == 0)
-		//	{
-		//		//Durch Vielfaches teilbar --> Vielfaches kommt auf die BlackList
-		//		//printf(" %d ist ein vielfaches von %d und somit ist %d keine Primzahl --> ", i, vielFaches, i);
-		//		bl.array[bl.used++] = i;
+		if (i == *max / 10) // bis 10%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 10%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 2) // bis 20%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 20%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 3) // bis 30%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 30%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 4) // bis 40%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 40%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 5) // bis 50%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 50%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 6) // bis 60%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 60%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 7) // bis 70%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 70%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 8) // bis 80%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 80%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == (*max / 10) * 9) // bis 90%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 90%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+		if (i == *max - 1) // bis 100%
+		{
+			clock_t end = clock();
+			time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("Benötigte Zeit bis 100%% %f seconds\n", time_spent);
+			time_spent = 0.0;
+			begin = clock();
+		}
+	}
+	return 0;
+}
 
-		//		//for (int k = vielFaches; k <= MAX_NUMBER; k = vielFaches + k)
-		//		//{
-		//		//	bl.array[bl.used++] = k;
-		//		//}
+int CreateErathotenes(int* _numbers, int* noPrimes)
+{
+	_numbers[0] = 0;
+	_numbers[1] = 0;
 
-		//		//bl.used++;
-		//		//printf("Vielfaches:(%d) wird der Blacklist hinzugefügt\n", vielFaches);
-		//		flag = 0;
-		//		//_numbers[i] = 0;
-		//	}
-		//}
+	//calcPrimes(0, MAX_NUMBER/2, _numbers, noPrimes);	
+	//calcPrimes(MAX_NUMBER/2, MAX_NUMBER, _numbers, noPrimes);
 
-		//(flag == 1) ? primeNumbers++ : NULL;
+	HANDLE tHandles[MAX_THREADS];
+	DWORD threads[MAX_THREADS];
+	PMYDATA pDataArray[MAX_THREADS];
+
+	HANDLE hMutex = CreateMutex(NULL, TRUE, "MutexExample");
+
+	for (int i = 0; i < MAX_THREADS; i++)
+	{
+		struct param myParam1;
+
+		int from = 0;
+		int til = MAX_NUMBER;
+		int max = MAX_NUMBER;
+
+		switch (i)
+		{
+		case 0:
+			from = 0;
+			til = MAX_NUMBER / 2;
+			break;
+		case 1:
+			from = MAX_NUMBER / 2;
+			til = MAX_NUMBER;
+			break;
+		default:
+			break;
+		}
+
+		pDataArray[i]->from = &from;
+		pDataArray[i]->til = &til;
+		pDataArray[i]->max = &max;
+		pDataArray[i]->_numbers = _numbers;
+		pDataArray[i]->noPrimes = noPrimes;
+
+		threads[i] = i;
+
+		printf("Thread %d gestartet. Von %d bis %d ThreadID (%u)\n", i, from, til, threads[i]);
+
+		/* create the thread */
+		tHandles[i] = CreateThread(
+			NULL,					/* default security attributes */
+			0,						/* default stack size */
+			calcPrimes,				/* thread function */
+			pDataArray[i],				/* parameter to thread function */
+			0,						/* default creation    flags */
+			&threads[i]);
 	}
 
-	//printf("Naechstes Vielfaches ist an der Reihe: %d\n\n", vielFaches);
+	WaitForMultipleObjects(MAX_THREADS, tHandles, TRUE, INFINITE);
 
-	freeArray(&bl);
-	return primeNumbers - 2;
+	for (size_t i = 0; i < MAX_THREADS; i++)
+	{
+		CloseHandle(tHandles[i]);
+	}
 }
 
 void FindPrimes()
@@ -174,8 +297,10 @@ void FindPrimes()
 	double time_spent = 0.0;
 	clock_t begin = clock();
 
-	//InitializeNumbers(_numbers.array);
-	int primesFound = CreateErathotenes(_numbers.array);
+	InitializeNumbers(_numbers.array); // ALle Stellen wurden mit 1 überschrieben
+	int noPrimes = 0;
+	CreateErathotenes(_numbers.array, &noPrimes);
+	int primesFound = MAX_NUMBER - noPrimes - 2;
 
 	clock_t end = clock();
 	// calculate elapsed time by finding difference (end - begin) and
@@ -190,54 +315,5 @@ void FindPrimes()
 void main() {
 
 	FindPrimes();
-
-	//int primesFound = 0;
-	//int* _primesFound = &primesFound;
-	//CalcPrimeNumbers(_primesFound, 0);
-
-	//HANDLE tHandles[5];
-	//DWORD threads[MAX_THREADS];
-
-	//for (int i = 0; i < MAX_THREADS; i++)
-	//{
-	//	HANDLE tHandle;
-	//	DWORD threadId;
-
-	//	// Parameterinitialisierung
-	//	struct param myParam;
-	//	myParam.counter = 5;
-	//	myParam.workerId = &threadId;
-	//	printf("<WorkerIdD: %ld>\n", &threadId);
-
-	//	// Pointer wird erzeugt
-	//	struct param* myParamPtr = &myParam;
-
-	//	/* create the thread */
-	//	tHandles[i] = CreateThread(
-	//		NULL,					/* default security attributes */
-	//		0,						/* default stack size */
-	//		WorkerFunction,			/* thread function */
-	//		myParamPtr,				/* parameter to thread function */
-	//		0,						/* default creation    flags */
-	//		&threads[i]);
-
-	//}
-
-	/////* returns the thread identifier */
-	////if (ThreadHandle != NULL) {
-	////	/* now wait for the thread to finish */ WaitForSingleObject(ThreadHandle, INFINITE);
-	////	/* close the thread handle */
-	////	CloseHandle(ThreadHandle);
-	////}
-
-	//WaitForMultipleObjects(MAX_THREADS, tHandles, TRUE, INFINITE);
-
-	//for (size_t i = 0; i < MAX_THREADS; i++)
-	//{
-	//	CloseHandle(tHandles[i]);
-	//	printf("%ld beendet.\n", tHandles[i]);
-	//	printf("%d beendet.\n", &tHandles[i]);
-	//}
-
 	hold();
 }
