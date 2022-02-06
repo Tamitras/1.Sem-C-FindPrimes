80f6fbc23f4cfa2c6206de6c4e3ce15c248898e8
Erik <erik.kaufmann91@gmail.com>
2022-02-06 11:07:40 +0100

    Added comments

--- DhbwProject/Prime.c
+++ DhbwProject/Prime.c
@@ -4,130 +4,136 @@
 #include <math.h>
 #include <windows.h>
 
-int num_threads = 4;
-long long max_number = 100000000;
+int num_threads = 4; // Threads
+unsigned long long max_number = 100000000; // MaxNumber
 
 typedef char sieve_t;
-static sieve_t* __restrict sieve;
+static sieve_t* __restrict sieve; // keyword to let the compiler do some optimizations for speed
 
 void initialize_sieve(void)
 {
-    sieve = (sieve_t*)calloc(max_number + 1, sizeof(sieve_t)); // 0 initialized
-    sieve[2] = 1;
-    register long long i = 3;
-    for (; i <= max_number; i += 2)
-        sieve[i] = 1;
+	sieve = (sieve_t*)calloc(max_number + 1, sizeof(sieve_t)); // 0 initialized
+	sieve[2] = 1; // Special case
+	for (register unsigned int i = 3; i <= max_number; i += 2)
+		sieve[i] = 1; // pre set all not even numbers to 1
 }
-
 
 DWORD WINAPI run_sieve_func(void* data)
 {
-    long long max_i = sqrt(max_number);
-    register long long i = *(int*)data;
-    for (; i <= max_i; i += 2 * num_threads)
-        if (sieve[i])
-        {
-            register long long k = i * i;
-            for (; k <= max_number; k += i)
-                if (sieve[k])
-                    sieve[k] = 0;
-        }
-    return 0;
-}
+	register unsigned int max_i = sqrt(max_number); // Get the root of MaxNumber
 
+	// Keyword register means that the variable is stored in cpu register instead of RAM --> faster location/access
+	for (register unsigned int i = *(int*)data; i <= max_i; i += 2 * num_threads)
+		if (sieve[i])
+			for (register unsigned int k = i * i; k <= max_number; k += i)
+				if (sieve[k])
+					sieve[k] = 0;
+
+	return 0;
+}
 
 void run_sieve(void)
 {
-    HANDLE* tHandles = (HANDLE*)malloc(num_threads * sizeof(HANDLE));
-    DWORD* threads = (DWORD*)malloc(num_threads * sizeof(DWORD));
-    int* data = (int*)malloc(num_threads * sizeof(int));
-    register int i = 0;
+	HANDLE* tHandles = (HANDLE*)malloc(num_threads * sizeof(HANDLE));
+	DWORD* threads = (DWORD*)malloc(num_threads * sizeof(DWORD));
+	int* data = (int*)malloc(num_threads * sizeof(int));
 
-    for (; i < num_threads; i++) {
-        data[i] = 3 + 2 * i;
-        threads[i] = i;
+	// Run with Threads
+	for (register unsigned int i = 0; i < num_threads; i++) {
+		data[i] = 3 + 2 * i;
+		threads[i] = i;
 
-        tHandles[i] = CreateThread(
-            NULL,
-            0,
-            run_sieve_func,
-            &data[i],
-            0,
-            &threads[i]
-        );
-    }
+		tHandles[i] = CreateThread(
+			NULL,
+			0,
+			run_sieve_func,
+			&data[i],
+			0,
+			&threads[i]
+		);
+	}
 
-    WaitForMultipleObjects(num_threads, tHandles, TRUE, INFINITE);
+	WaitForMultipleObjects(num_threads, tHandles, TRUE, INFINITE);
 
-    i = 0;
-    for (; i < num_threads; i++) {
-        CloseHandle(tHandles[i]);
-    }
+	for (int i = 0; i < num_threads; i++) {
+		CloseHandle(tHandles[i]);
+	}
 
-    free(threads);
-    free(tHandles);
+	free(threads);
+	free(tHandles);
 }
+
 
 long count_primes(void)
 {
-    long count = 0;
-    register long long i = 1;
-    for (; i <= max_number; i++)
-        count += sieve[i];
+	long count = 0;
+	for (register long long i = 1; i <= max_number; i++)
+		count += sieve[i];
 
-    return count;
+	return count;
 }
+
 
 int main(int argc, char* argv[])
 {
-    if (argc > 1) {
-        num_threads = atoi(argv[1]);
-    }
+	if (argc > 1) {
+		num_threads = atoi(argv[1]); // char to integer (ato[I]nt)
+	}
 
-    if (argc > 2) {
-        max_number = atoll(argv[2]);
-    }
+	if (argc > 2) {
+		max_number = atoll(argv[2]); // char to long long (ato[L]ong[L]ong)
+	}
 
-    printf("num_threads: %d\n", num_threads);
-    printf("max_number: %lld\n", max_number);
+	// Print arguments
+	printf("%-56s %d\n", "\nThreads:", num_threads);
+	printf("%-55s %d\n", "Max Number:", max_number);
 
-    double time_spent, total_time;
-    clock_t begin, end;
-    total_time = 0.0;
+	// Declare local variables
+	double time_spent, total_time;
+	clock_t begin, end;
+	total_time = 0.0;
 
-    printf("---------------------------------------------------------------------\n");
+	printf("----------------------------------------------------------------------\n");
 
-    // Initialize sieve
-    begin = clock();
-    initialize_sieve();
-    end = clock();
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    total_time += time_spent;
-    printf("%-55s %.3f seconds\n", "Initialization time:", time_spent);
+	// Initialize sieve of Erathostenes
+	begin = clock();
+	initialize_sieve();
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	total_time += time_spent;
+	printf("%-55s %.4f seconds\n", "Initialization time:", time_spent); // Print init time with 4 digits floating point and after 55 digits in console.
 
-    // Run sieve
-    begin = clock();
-    run_sieve();
-    end = clock();
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    total_time += time_spent;
-    printf("%-55s %.3f seconds\n", "Calculation time:", time_spent);
+	// Run sieve
+	begin = clock();
+	run_sieve();
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	total_time += time_spent;
+	printf("%-55s %.4f seconds\n", "Calculation time:", time_spent);
 
-    // Count primes
-    begin = clock();
-    long count = count_primes();
-    end = clock();
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    total_time += time_spent;
-    printf("%-55s %.3f seconds\n", "Counting primes time:", time_spent);
+	// Count primes
+	begin = clock();
+	long count = count_primes();
+	end = clock();
+	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	total_time += time_spent;
+	printf("%-55s %.4f seconds\n", "Counting primes time:", time_spent);
 
-    printf("---------------------------------------------------------------------\n");
+	printf("----------------------------------------------------------------------\n");
 
-    printf("%-55s %.3f seconds\n", "Total time:", total_time);
-    printf("\n");
-    printf("Primes found = %ld\n", count);
+	printf("%-55s %.4f seconds\n", "Total time:", total_time);
+	printf("\n");
 
-    free(sieve);
+	if (count == 5761455 || count == 50847534) // 10^8 or 10^9 
+	{
+		printf("Primes found = %ld\n", count);
+	}
+	else
+	{
+		printf("!!! Something went wrong !!!\n");
+	}
 
-    return 0;
+	free(sieve); // deallocation memory of sieve
+
+	return 0;
 }
