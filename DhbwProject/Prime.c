c1c20f29b24662551d548fab55bdd9f5bafe4052
Erik <erik.kaufmann91@gmail.com>
2022-05-10 19:57:37 +0200

    things...

--- DhbwProject/Prime.c
+++ DhbwProject/Prime.c
@@ -2,71 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <windows.h>
+#include <windows.h>														// required for ThreadHandling
 
-int num_threads = 4; // threads
-long long max_number = 100000000; // maxNumber
+int num_threads = 4;														// threads
+long long max_number = 100000000;											// maxNumber
 
-typedef char sieve_t; // change to int decrease performance 
-static sieve_t* __restrict sieve; // keyword to let the compiler do some optimizations for speed
+typedef char sieve_t;														// change to int decrease performance 
+static sieve_t* __restrict sieve;											// keyword to let the compiler do some optimizations for speed
 HANDLE* tHandles;
 DWORD* threads;
-int* data;
+int* oddNumbers;
 
 void initialize_sieve(void)
 {
-	sieve[2] = 1; // special case
-	for (register unsigned int i = 3; i <= max_number; i += 2)
-		sieve[i] = 1; // pre set all not even numbers to 1
+	sieve[2] = 1;															// special case
+	for (register unsigned long long i = 3; i <= max_number; i += 2)
+		sieve[i] = 1;														// pre set all not even numbers to 1
 }
 
-void run_sieve_func(void* data) // Null Pointer as parameter
+void run_sieve_func(void* data)												// Null Pointer as parameter
 {
-	int max_i = sqrt(max_number); // Get the root of MaxNumber
-	int _data = *(int*)data; // cast void pointer to int pointer an get value of this pointer
-
-	// Keyword register means that the variable is stored in cpu register instead of RAM --> faster location/access
-	for (register int i = _data; i <= max_i; i += 2 * num_threads)
+	long long max_i = sqrt(max_number);										// Get the root of MaxNumber
+	int _data = *(int*)data;												// cast void pointer to int pointer an get value of this pointer
+	
+	for (register long long i = _data; i <= max_i; i += 2 * num_threads)	// Keyword register means that the variable is stored in cpu register instead of RAM --> faster location/access
 		if (sieve[i])
-			for (register int k = i * i; k <= max_number; k += i)
+			for (register long long k = i * i; k <= max_number; k += i)
 				if (sieve[k]) sieve[k] = 0;
 }
 
 void run_sieve(void)
 {
-	// Run with Threads
-	for (register int i = 0; i < num_threads; i++) {
-		data[i] = 3 + 2 * i; // Database of odd numbers
-		//printf("Thread started with: %d and offset: %d\n", 3 + 2 * i, i);
+	for (register long long i = 0; i < num_threads; i++) {					// Run with Threads
+		oddNumbers[i] = 3 + 2 * i;											// Database of odd numbers
 		threads[i] = i;
 
 		tHandles[i] = CreateThread(
 			NULL,
 			0,
 			run_sieve_func,
-			&data[i],
+			&oddNumbers[i],
 			0,
 			&threads[i]
 		);
 	}
 
-	// Wait for Threads to be finished
-	WaitForMultipleObjects(num_threads, tHandles, TRUE, INFINITE);
-
+	WaitForMultipleObjects(num_threads, tHandles, TRUE, INFINITE);			// Wait for Threads to be finished
 	for (int i = 0; i < num_threads; i++) {
-		// Close all open ThreadHandles
-		CloseHandle(tHandles[i]);
+		CloseHandle(tHandles[i]);											// Close all open ThreadHandles
 	}
-
-	// deallocation memory of threads and tHandles
-	free(threads);
+	
+	free(threads);															// deallocation memory of threads and tHandles
 	free(tHandles);
 }
 
 long count_primes(void)
 {
 	long count = 0;
-	for (register int i = 1; i <= max_number; i++)
+	for (register long long i = 1; i <= max_number; i++)
 		count += sieve[i];
 
 	return count;
@@ -74,20 +67,16 @@ long count_primes(void)
 
 int main(int argc, char* argv[])
 {
-	if (argc > 1) {
-		num_threads = atoi(argv[0]); // char to integer (ato[I]nt)
-	}
+	if (argc > 1) 
+		num_threads = atoi(argv[0]);										// char to integer (ato[I]nt)
 
-	if (argc > 2) {
-		max_number = atoll(argv[1]); // char to long long (ato[L]ong[L]ong)
-	}
+	if (argc > 2) 
+		max_number = atoll(argv[1]);										// char to long long (ato[L]ong[L]ong)
 
-	// Print arguments
-	printf("%-56s %d\n", "\nThreads:", num_threads);
+	printf("%-56s %d\n", "\nThreads:", num_threads);						// Print arguments
 	printf("%-55s %lld\n", "Max Number:", max_number);
 
-	// Declare local variables
-	double time_spent, total_time;
+	double time_spent, total_time;											// Declare local variables
 	clock_t begin, end;
 	total_time = 0.0;
 
@@ -95,11 +84,12 @@ int main(int argc, char* argv[])
 	typedef uint8_t BYTE;
 	typedef uint16_t WORD;
 	typedef uint32_t DWORD;
-	typedef uint64_t QWORD;*/
-	sieve = (sieve_t*)calloc(max_number + 1, sizeof(sieve_t)); // 0 initialized
+	typedef uint64_t QWORD;
+	*/
+	sieve = (sieve_t*)calloc(max_number + 1, sizeof(sieve_t));				// 0 initialized
 	tHandles = (HANDLE*)malloc(num_threads * sizeof(HANDLE));
 	threads = (DWORD*)malloc(num_threads * sizeof(DWORD));
-	data = (int*)malloc(num_threads * sizeof(int));
+	oddNumbers = (int*)malloc(num_threads * sizeof(int));
 
 	printf("----------------------------------------------------------------------\n");
 
@@ -109,7 +99,7 @@ int main(int argc, char* argv[])
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	total_time += time_spent;
-	printf("%-55s %.4f seconds\n", "Initialization time:", time_spent); // Print init time with 4 digits floating point and after 55 digits in console.
+	printf("%-55s %.4f seconds\n", "Initialization time:", time_spent);		// Print init time with 4 digits floating point and after 55 digits in console.
 
 	// Run sieve
 	begin = clock();
@@ -133,17 +123,33 @@ int main(int argc, char* argv[])
 	printf("\n");
 
 	if (count == 5761455 || count == 50847534) // 10^8 or 10^9 
-	{
 		printf("Primes found = %ld\n", count);
-	}
 	else
-	{
 		printf("!!! Something went wrong !!!\n");
-	}
+
+	//		10^8 (100.000.000)
+	//								Init:	 0.01sec
+	//								Calc:	 0.42sec
+	//								Sum:	 0.01sec
+	// 
+	//								Gesamt: 0.44sec
+	
+	//		10^9 (1.000.000.000)
+	//								Init:	 0.17sec
+	//								Calc:	 5.35sec
+	//								Sum:	 0.10sec
+	// 
+	//								Gesamt:  5.50sec
+	
+	//		10^10	(10.000.000.000)		
+	//								Init:	 2.50sec
+	//								Calc:	80.57sec
+	//								Sum:	 1.30sec
+	// 
+	//								Gesamt: 85.00sec
 
 	free(sieve); // deallocation memory of sieve
-
-	//scanf_s("%d", &count);
+	scanf("%d", &count);
 
 	return 0;
 }
